d76471d33c5c568928c14638f3f7e3aa9793ea8a
farshad <farshad@qburst.com>
2016-01-06 13:04:18 +0530

    master: Simple server setup

---
+++ webserver.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    fs = require('fs');
+
+console.log("Starting web server at port 8000");
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8000);
+});
